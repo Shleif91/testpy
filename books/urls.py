3ffbca4da0197283c300f127edda1952aef5843f
rofar91 <rofar91@gmail.com>
2018-01-14 22:14:48 +0300

    zaebalsy yzge

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url('books/', views.books_list, name='books_list'),
+    url('', views.post_list, name='post_list'),
 ]
--- books/views.py
+++ books/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'books/post_list.html', {})
+
+
+def books_list(request):
+    return render(request, 'books/books_list.html', {})
