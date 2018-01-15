76973a1a347e515f314f77427f18de612a10f74c
rofar91 <rofar91@gmail.com>
2018-01-15 22:12:44 +0300

    Create book tamplate

--- books/views.py
+++ books/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Book
 
 def post_list(request):
     return render(request, 'books/post_list.html', {})
 
 
 def books_list(request):
-    return render(request, 'books/books_list.html', {})
+    books = Book.objects.all()
+    return render(request, 'books/books_list.html', {'all_books': books})
